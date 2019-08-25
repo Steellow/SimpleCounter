b47ba37382cdc9208a0c07b0fdc1dfb3b305d793
Steellow <hankilapio@gmail.com>
2019-08-25 11:19:27 +0300

    Added feature 'double click back to exit'

--- app/src/main/java/com/hankiapps/simplecounter/MainActivity.java
+++ app/src/main/java/com/hankiapps/simplecounter/MainActivity.java
@@ -1,11 +1,29 @@
 package com.hankiapps.simplecounter;
 
 import android.support.v4.app.Fragment;
+import android.widget.Toast;
+
+import java.util.logging.Handler;
 
 public class MainActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
         return new fragment_main();
+    }
+
+
+    //Double click back to exit
+    private static final int time_interval = 2000;
+    private long backPressed;
+
+    @Override
+    public void onBackPressed() {
+        if(backPressed + time_interval > System.currentTimeMillis()) {
+            super.onBackPressed();
+            return;
+        } else {
+            backPressed = System.currentTimeMillis();
+        }
     }
 }
